5fcd95780095bf81a38b468ba9a17f89e6789c07
blast-sky <strogancev2020@gmail.com>
2023-05-20 17:51:36 +0300

    update versions and add version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:8.0.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.45")
     }
 }
 plugins {
     id("com.android.application") version "7.2.1" apply false
     id("com.android.library") version "7.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    alias(stack.plugins.kotlin.android) apply false
 }
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
